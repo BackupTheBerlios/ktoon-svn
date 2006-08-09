56cb0f3e3a61de96add3af038a6b0977c9a65204
kuadrosx <kuadrosx@44319fdc-33ef-0310-b156-c2c9286b3847>
2006-08-09 22:38:50 +0000

    added files

---
+++ trunk/ktoon/src/plugins/tools/select/nodemanager.cpp
@@ -0,0 +1,111 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Jorge Cuadrado                                  *
+ *   kuadrosx@toonka.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "nodemanager.h"
+
+#include "QDebug"
+NodeManager::NodeManager(QGraphicsItem * parent, KTScene *scene): m_parent(parent)
+{
+	QRectF rect = parent->sceneBoundingRect();
+	Node *topLeft = new Node(Node::TopLeft, rect.topLeft(), this, parent, scene);
+	Node *topRight = new Node(Node::TopRight, rect.topRight(), this, parent, scene);
+	Node *bottomLeft = new Node(Node::BottomLeft, rect.bottomLeft(),this, parent, scene);
+	Node *bottomRight = new Node(Node::BottomRight, rect.bottomRight(),this, parent, scene);
+	Node *center = new Node(Node::Center, rect.center() ,this,parent, scene);
+	m_nodes.insert(Node::TopLeft, topLeft);
+	m_nodes.insert(Node::TopRight, topRight);
+	m_nodes.insert(Node::BottomLeft, bottomLeft );
+	m_nodes.insert(Node::BottomRight, bottomRight );
+	m_nodes.insert(Node::Center, center );
+}
+
+
+
+NodeManager::~NodeManager()
+{
+	foreach(Node *node, m_nodes)
+	{
+		if(node)
+		{
+			node->scene()->removeItem(node);
+		}
+		delete node;
+		node = 0;
+	}
+	m_nodes.clear();
+	
+}
+
+void NodeManager::syncNodes(const QRectF sbr)
+{
+	if(m_nodes.isEmpty())
+	{
+		return;
+	}
+	QHash<Node::TypeNode, Node *>::iterator it = m_nodes.begin();
+	while(it != m_nodes.end())
+	{
+		if((*it))
+		{
+		switch(it.key())
+		{
+			case Node::TopLeft:
+			{
+				(*it)->setPos( sbr.topLeft() );
+				break;
+			}
+			case Node::TopRight:
+			{
+				(*it)->setPos(sbr.topRight());
+				break;
+			}
+			case Node::BottomRight:
+			{
+				(*it)->setPos(sbr.bottomRight());
+				break;
+			}
+			case Node::BottomLeft:
+			{
+				(*it)->setPos(sbr.bottomLeft());
+				break;
+			}
+			case Node::Center:
+			{
+				(*it)->setPos(sbr.center());
+				break;
+			}
+		}
+		}
+		++it;
+	}
+}
+
+void NodeManager::syncNodesFromParent()
+{
+	if(m_parent)
+	{
+		syncNodes(m_parent->sceneBoundingRect());
+	}
+}
+
+QGraphicsItem *NodeManager::parentItem() const
+{
+	return m_parent;
+}
+
