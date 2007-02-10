c87840576f16b035e97cadc66febc25ad1ab1454
kuadrosx <kuadrosx@44319fdc-33ef-0310-b156-c2c9286b3847>
2007-02-10 02:30:37 +0000

    added test

---
+++ trunk/ktoond/src/tests/testproject.cpp
@@ -0,0 +1,24 @@
+#include <QtTest/QtTest>
+#include <QDomDocument>
+#include <project.h>
+
+class TestProject: public QObject
+{
+	Q_OBJECT
+	private slots:
+		void createProject();
+};
+
+void TestProject::createProject()
+{
+	bool result = false;
+	
+	
+	Packages::Project project("project1.ktn");
+	
+	
+	QCOMPARE( result, project.isValid());
+}
+
+QTEST_MAIN(TestProject)
+#include "testproject.moc"
---
+++ trunk/ktoond/src/tests/testprojectlist.cpp
@@ -0,0 +1,25 @@
+#include <QtTest/QtTest>
+#include <QDomDocument>
+#include <projectlist.h>
+
+class TestProjectList: public QObject
+{
+	Q_OBJECT
+	private slots:
+		void createProjectList();
+};
+
+void TestProjectList::createProjectList()
+{
+	QDomDocument result;
+	result.setContent(QString("<projectlist><project description=\"animaciones de prueba\" author=\"Animaciones S.A.\" name=\"proyecto 1\"/></projectlist>"));
+	
+	
+	Packages::ProjectList projectlist;
+	projectlist.addProject("proyecto 1", "Animaciones S.A.", "animacion de prueba");
+	
+	QCOMPARE( result.toString(0), projectlist.toString(0));
+}
+
+QTEST_MAIN(TestProjectList)
+#include "testprojectlist.moc"
