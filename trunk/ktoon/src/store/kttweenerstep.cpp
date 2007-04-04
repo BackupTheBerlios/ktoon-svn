7b809bb7851bd9ecc24bdb0f1396277e73c3e25f
krawek <krawek@44319fdc-33ef-0310-b156-c2c9286b3847>
2007-04-04 16:40:12 +0000

    up frames to steps.count() if it is invalid

--- trunk/ktoon/src/store/kttweenerstep.cpp
+++ trunk/ktoon/src/store/kttweenerstep.cpp
@@ -231,6 +231,11 @@ void KTTweenerStep::fromXml(const QString& xml)
 
 QDomDocument KTTweenerStep::createXml(int frames, const QVector<KTTweenerStep *> &steps)
 {
+	if( frames < steps.count() )
+	{
+		frames = steps.count();
+	}
+	
 	QDomDocument doc;
 	
 	QDomElement root = doc.createElement("steps");
