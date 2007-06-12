ab25018e89e774b7758dd46ea1b7241a8028fe7c
xtingray <xtingray@44319fdc-33ef-0310-b156-c2c9286b3847>
2007-06-12 21:28:56 +0000

    Renaming _install_ dir to _launcher_

--- trunk/ktoon/src/shell/main.cpp
+++ trunk/ktoon/src/shell/main.cpp
@@ -52,6 +52,15 @@
 
 #include <dgui/dcollapsiblewidget.h>
 
+#ifndef CONFIG_H
+#define CONFIG_H
+#define HAVE_SOUND
+#define HAVE_LIBGIF
+#define HAVE_FFMPEG
+#define VERSION 0.9+svn
+#define VERSION_STR "0.9+svn"
+#endif
+
 void usage();
 
 int main( int argc, char ** argv )
