0a59611a009e357e4305815015645e64bfc50dbb
xtingray <xtingray@44319fdc-33ef-0310-b156-c2c9286b3847>
2010-04-01 04:57:54 +0000

    Just editing code headers

--- trunk/kom/configure.tests/ffmpeg/main.cpp
+++ trunk/kom/configure.tests/ffmpeg/main.cpp
@@ -1,3 +1,33 @@
+/***************************************************************************
+ *   Project KOM: KToon Open Media library                                 *
+ *   Project Contact: ktoon@labtoon.org                                    *
+ *   Project Website: http://www.ktoon.net                                 *
+ *   Project Leader: Gustavo Gonzalez <xtingray@ktoon.net>                 *
+ *                                                                         *
+ *   Developers:                                                           *
+ *   2010:                                                                 * 
+ *    Gustavo Gonzalez                                                     *
+ *   2006:                                                                 *
+ *    David Cuadrado                                                       *
+ *    Jorge Cuadrado                                                       *
+ *                                                                         *
+ *   License:                                                              *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifdef __cplusplus
 extern "C" {
 #include "libavformat/avformat.h"
@@ -9,30 +39,24 @@ extern "C" {
 
 int main()
 {
-	av_register_all();
-	AVOutputFormat *fmt = guess_format("mpeg", NULL, NULL);
-	
-	//AVFormatContext *oc = av_alloc_format_context();
-        AVFormatContext *oc = avformat_alloc_context();
-	
-	oc->oformat = fmt;
-	
-	AVCodecContext *c;
-	AVStream *st;
-	
-	st = av_new_stream(oc, 0);
-	
-	c = st->codec;
-	
-	c->time_base.den = 24;
-	c->time_base.num = 1;
-	c->gop_size = 12;
-	c->pix_fmt = PIX_FMT_YUV420P;
-	
-	
-	av_free(oc);
-	
-	
-	return 0;
+    av_register_all();
+    AVOutputFormat *fmt = guess_format("mpeg", NULL, NULL);
+    AVFormatContext *oc = avformat_alloc_context();
+    
+    oc->oformat = fmt;
+    
+    AVCodecContext *c;
+    AVStream *st;
+    
+    st = av_new_stream(oc, 0);
+    
+    c = st->codec;
+    c->time_base.den = 24;
+    c->time_base.num = 1;
+    c->gop_size = 12;
+    c->pix_fmt = PIX_FMT_YUV420P;
+    
+    av_free(oc);
+    
+    return 0;
 }
-
--- trunk/kom/configure.tests/opengl/main.cpp
+++ trunk/kom/configure.tests/opengl/main.cpp
@@ -1,3 +1,32 @@
+/***************************************************************************
+ *   Project KOM: KToon Open Media library                                 *
+ *   Project Contact: ktoon@labtoon.org                                    *
+ *   Project Website: http://www.ktoon.net                                 *
+ *   Project Leader: Gustavo Gonzalez <xtingray@ktoon.net>                 *
+ *                                                                         *
+ *   Developers:                                                           *
+ *   2010:                                                                 * 
+ *    Gustavo Gonzalez                                                     *
+ *   2006:                                                                 *
+ *    David Cuadrado                                                       *
+ *    Jorge Cuadrado                                                       *
+ *                                                                         *
+ *   License:                                                              *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
 
 #include <QGLWidget>
 #include <QApplication>
@@ -5,11 +34,8 @@
 
 int main(int argc, char **argv)
 {
-	QApplication app(argc, argv);
-	
-	QGLWidget w;
-	
-	
-	return app.exec();
-}
+    QApplication app(argc, argv);
+    QGLWidget w;
 
+    return app.exec();
+}
