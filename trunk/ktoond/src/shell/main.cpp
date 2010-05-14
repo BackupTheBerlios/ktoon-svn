2ff8ad7fba3872637f494ac9f9e1105b87b86895
xtingray <xtingray@44319fdc-33ef-0310-b156-c2c9286b3847>
2010-05-14 05:56:21 +0000

    Just identing code

--- trunk/ktoond/src/shell/main.cpp
+++ trunk/ktoond/src/shell/main.cpp
@@ -1,21 +1,30 @@
 /***************************************************************************
- *   Copyright (C) 2006 by David Cuadrado                                  *
- *   krawek@toonka.com                                                     *
+ *   Project KTOOND: KToon Collaboration Daemon                            *
+ *   Project Contact: ktoon@labtoon.org                                    *
+ *   Project Website: http://www.ktoon.net                                 *
+ *   Project Leader: Gustavo Gonzalez <xtingray@ktoon.net>                 *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   Developers:                                                           *
+ *   2010:                                                                 *
+ *    Gustavo Gonzalez                                                     *
+ *   2006:                                                                 *
+ *    David Cuadrado                                                       *
+ *    Jorge Cuadrado                                                       *
+ *                                                                         *
+ *  License:                                                               *
+ *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation, either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    This program is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *  
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
  ***************************************************************************/
 
 #include <QApplication>
@@ -57,7 +66,7 @@ int main(int argc, char **argv)
     if (!KCONFIG->isOk()) {
         KCONFIG->beginGroup("Connection");
         KCONFIG->setValue("host", "127.0.0.1");
-        KCONFIG->setValue("port", "6502");
+        KCONFIG->setValue("port", "2020");
         
         KCONFIG->beginGroup("General");
         KCONFIG->setValue("repository", kAppProp->configDir() + "/repository");
@@ -76,7 +85,7 @@ int main(int argc, char **argv)
     if (!cache.exists ())
         cache.mkdir(kAppProp->cacheDir());
     
-    QString dbdir = kAppProp->configDir()+"/database";
+    QString dbdir = kAppProp->configDir() + "database";
     QDir db(dbdir);
     if (!db.exists()) 
         db.mkdir(dbdir);
