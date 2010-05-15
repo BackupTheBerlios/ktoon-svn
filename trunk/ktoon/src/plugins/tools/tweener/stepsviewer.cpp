0c9ee75556fa18efba61bb9a140ede700e7819d2
xtingray <xtingray@44319fdc-33ef-0310-b156-c2c9286b3847>
2010-05-15 21:49:57 +0000

    Updating license version from source code -> GPL 3

--- trunk/ktoon/src/plugins/tools/tweener/stepsviewer.cpp
+++ trunk/ktoon/src/plugins/tools/tweener/stepsviewer.cpp
@@ -1,12 +1,23 @@
 /***************************************************************************
- *   Project KTOON: 2D Animation Toolkit 0.9                               *
- *   Project Contact: ktoon@toonka.com                                     *
- *   Project Website: http://ktoon.toonka.com                              *
- *   Copyright (C) 2007 by Jorge Cuadrado <kuadrosx@toonka.com>            *
+ *   Project KTOON: 2D Animation Toolkit 0.9a                              *
+ *   Project Contact: ktoon@labtoon.org                                    *
+ *   Project Website: http://www.ktoon.net                                 *
+ *   Project Leader: Gustavo Gonzalez <xtingray@ktoon.net>                 *
  *                                                                         *
+ *   Developers:                                                           *
+ *   2010:                                                                 * 
+ *    Gustavo Gonzalez                                                     *
+ *   2006:                                                                 *
+ *    David Cuadrado                                                       *
+ *    Jorge Cuadrado                                                       *
+ *   2003:                                                                 *
+ *    Fernado Roldan                                                       * 
+ *    Simena Dinas                                                         *
+ *                                                                         *
+ *   License:                                                              *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
@@ -15,9 +26,7 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
 
 #include <QGraphicsPathItem>
