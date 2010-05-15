c8d62ce056764c7d01956e56184298f23c0e73fc
xtingray <xtingray@44319fdc-33ef-0310-b156-c2c9286b3847>
2010-05-15 21:48:51 +0000

    Updating license version from source code -> GPL 3

--- trunk/kom/kom/kcore/kmd5hash.h
+++ trunk/kom/kom/kcore/kmd5hash.h
@@ -18,7 +18,7 @@
 ** This file is part of the OSDaB project (http://osdab.sourceforge.net/).
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
+** GNU General Public License version 3 as published by the Free Software
 ** Foundation and appearing in the file LICENSE.GPL included in the
 ** packaging of this file.
 **
